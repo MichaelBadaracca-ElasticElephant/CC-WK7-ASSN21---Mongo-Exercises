c61335362b67c8efe62bf2820a14e3b5e0b19add
Michael Badaracca - Elastic Elephant <michael.badaracca@elasticElephant.com>
2017-05-28 15:42:35 -0600

    Added initial assignment files

---
+++ mongoExercises/exercises/exercise_1.js
@@ -0,0 +1,5 @@
+module.exports = function(db) {
+	// How many movies are there?
+
+	console.log("Exercise 1:\n\tThere are X movies");
+};
---
+++ mongoExercises/exercises/exercise_2.js
@@ -0,0 +1,5 @@
+module.exports = function(db) {
+	// Which users checked out any of the Lord of the Rings trilogy?
+
+	console.log("Exercise 2:\n\tThe LOTR movies were checked out by users X, Y, ...")
+};
